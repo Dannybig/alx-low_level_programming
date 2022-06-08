123ae24d5bb0b22da7707688ed3e87133f0fec57
Tommy Awodokun <awodokuntomiwa@gmail.com>
2022-06-08 04:54:51 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
-#include <_putchar.h>
+include "main.h"
 /**
-* main - debugging example
-* Return: 0
+*main - Entry point
+*
+*Return: Always 0 (ok)
 */
 int main(void)
 {
-char *holberton = "Holberton";
-while (*holberton)
-{
-_putchar(*holberton);
-holberton++;
-}
+_putchar(_);
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
