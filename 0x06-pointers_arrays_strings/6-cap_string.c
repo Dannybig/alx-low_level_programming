3ac5be96abdd8abba93d07169a0bb79c4b2da92c
Tommy Awodokun <awodokuntomiwa@gmail.com>
2022-06-16 07:20:49 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,11 +8,11 @@
 char *cap_string(char *str)
 {
 int index = 0;
-while (str[index])
+while(str[index])
 {
-while (!(str[index] >= 'a' && str[index] <= 'z'))
+while(!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
-if (str[index - 1] == ' ' ||
+if(str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
 str[index - 1] == ',' ||
@@ -29,5 +29,5 @@ index == 0)
 str[index] -= 32;
 index++;
 }
-return (0);
+return (str);
 }
